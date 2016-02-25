18cd9d2aef2e5c2ad2512f976f0baa18530e2290
gerak1925 <ikarous1925@gmail.com>
2016-02-25 11:04:17 +0200

    js edit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,47 @@
-$(document).ready(function() {
+$(document).ready( function (){
+  var size;
+  function sketchSize(size) {
+    var sketchGrid = (size * 32);
+    var container = $("<div class='innerContainer'></div>");
+    for (var i = 0; i < size; i++) { 
+      for (var j = 0; j < size; j++) { 
+      container.append($("<div></div>").addClass("newDiv")).css({"width": sketchGrid, "height": sketchGrid});
+      }
+    }
+    $(".container").html(container);
+  }
+  function askSize() {
+    var size = prompt("Specify the quantity of divs");
+    if ((size > 0) && (size <= 16)) {
+      sketchSize(size);
+    } else {
+      alert("Invalid values, please put another value"); 
+    }    
+  }  
 
-	var pixel = $('#pixel');
+  $('#new-grid').on('click', function(){
+    askSize();
+    $('.newDiv').on('mouseenter', function(){
+      $(this).css("background-color", "blue");
+    });
+  });
 
-	for (var i = 0; i < 2500; i++) {
-		$('#inner').append('<div id="pixel"></div>');
-	};
+  $('#random').on('click', function(){
+    askSize();
+    $('.newDiv').on('mouseenter', function(){
+      var back = ["red","blue","gray", "green", "yellow", "orange"];
+      var rand = back[Math.floor(Math.random() * back.length)];
+      $(this).css('background',rand);
+    });
+  });
 
-	$("#pixel").hover(function() {
-		$(this).addClass('entering');
-	});
-	
+  $('#faded').on('click', function(){
+    askSize();
+    $('.newDiv').on('mouseenter', function(){
+      var opac = $(this).css('opacity');
+      var fade = opac - 0.1;
+      if (fade > 0) { $(this).css("opacity", fade); }
+      else {$(this).css("opacity", 0);}      
+    });
+  });
 });
